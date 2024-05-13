41869a518cd77c7914261b0d1439e1057f9361a0
pedx-ko <pedrxr2@gmail.com>
2024-05-13 11:04:58 +0200

    Day 5 challgenge done

--- day_5/password.py
+++ day_5/password.py
@@ -1,6 +1,5 @@
 from random import randint, shuffle
 import random
-from textwrap import shorten
 
 
 
@@ -14,27 +13,30 @@ symbols = ['!', '@', '#', '$', '%', '&', '*', '(', ')', '-', '+', '=', '?', '/',
 numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
-letters_to_select = 12
+letters_to_select = 12  # here i can use a input for the user put his preferences
 simbols_to_select = 9
 numbers_to_select = 2
 
-selecting_alp = alphabet[randint(0,len(alphabet)-1)]
-selecting_sym = symbols[randint(0,len(symbols)-1)]
-selecting_num = numbers[randint(0,len(numbers)-1)]
 
-
-passwos = ""
+#bulding the paswword
+passwos = []
 for i in range(letters_to_select):
-    so = (alphabet[randint(0,len(alphabet)-1)])
-    passwos += so
-
-
+    # passwos.append(alphabet[randint(0,len(alphabet)-1)])
+    passwos.append(random.choice(alphabet)) #choise is better for understanding code
+    
 for i in range(simbols_to_select):
-    so =(symbols[randint(0,len(symbols)-1)])
-    passwos += so
+    passwos.append(symbols[randint(0,len(symbols)-1)])
     
 for i in range(numbers_to_select):
-    so = str((numbers[randint(0,len(numbers)-1)]))
-    passwos += so
-    
-print(passwos)
+    passwos.append((numbers[randint(0,len(numbers)-1)]))
+
+#ramdon order   
+random.shuffle(passwos)
+
+#converting to string for printing as a Password
+
+passs = ""
+for i in passwos:
+    passs += str(i)
+    # print(i)
+print(passs)
