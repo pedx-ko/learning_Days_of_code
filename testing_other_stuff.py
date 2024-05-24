4d1789d0b70a982effc04126760c080cb4516fca
pedx-ko <pedrxr2@gmail.com>
2024-05-24 14:17:42 +0000

    May 24, 2024, 4:17 PM 16

--- testing_other_stuff.py
+++ testing_other_stuff.py
@@ -1,13 +1,20 @@
-class MarterMind:
-    def __init__(self, cobra) -> None:
-        
-        self.cobra = cobra
-        
+from random import randint
+
+
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def saludar(self):
+        return f"Hola, mi nombre es {self.nombre} y tengo {self.edad} años"
 
 
 
 
+juan = Persona("Juan", randint(25, 60))
+Marta = Persona("Marta",randint(20,60))
 
-trata = MarterMind(22)
-
-print(type(trata))
+print(juan.saludar())
+print("")
+print(Marta.saludar())
