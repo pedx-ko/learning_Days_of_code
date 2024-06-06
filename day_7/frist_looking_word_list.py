23cbdec7a69c5ee5a2589bb02ce3650b60fd9fd2
pedx-ko <pedrxr2@gmail.com>
2024-06-06 13:22:21 +0200

    "add ASCII art"

--- day_7/frist_looking_word_list.py
+++ day_7/frist_looking_word_list.py
@@ -2,6 +2,7 @@ import pretty_errors  # noqa: F401
 import os
 from itertools import count
 import random
+from imagg import stages, Intro_im, game_over
 
 words_list = ["mak silvester", "mari mar",
               "apple banana", "cherry", "grape mango"]
@@ -10,14 +11,17 @@ random_word = random.choice(words_list)
 # no_spaces = random_word.replace(" ", "")
 random_word = words_list[3]
 spaces_on = len(random_word)
-lives_on = 3
+lives_on = 6
 
-# TODO print this: _
+
+# TODO creating the spaces _
 spaces = ""
 for i in range(spaces_on):
     spaces += "_"
-print()
-print(f"{spaces}\n")
+
+
+print(Intro_im)
+
 
 # # TODO look if a letter--done
 # print("########### WELLL CCOOOMEE ########### \n")
@@ -26,8 +30,12 @@ while lives_on > 0:
     print(random_word)
     print()
     input_word = input("\n select a word: ").lower()
+
+    print()
+    print(f"{spaces}\n")
+
     os.system('cls')  # Call the clear() function to clear the console
-    if spaces.count("_") > 1:
+    if spaces.count("_") > 0:
         if input_word not in spaces.lower():
             if input_word in random_word:
                 for i in range(spaces_on):
@@ -36,11 +44,12 @@ while lives_on > 0:
                         spaces[i] = random_word[i]
                         # convert in to a strig to prin it
                         spaces = "".join([i for i in spaces])
-                print(f"\n \nlife = {lives_on} \n")
+                print(f"\n \n♥ LIFE = {lives_on} \n")
                 print(spaces)
             else:
                 lives_on -= 1
-                print(f"\n \n life = {lives_on}\n ")
+                print(f"\n \n♥ LIFE = {lives_on}\n ")
+                print(stages[lives_on])
                 print(spaces)
         else:
             print("\n Is allready there try again")
@@ -50,5 +59,7 @@ while lives_on > 0:
 
 if lives_on <= 0:
     os.system('cls')  # Call the clear() function to clear the console
-    print("\n \n GAME OVER")
-print(f"\n \nThe words is: {random_word }")
+    print(stages[lives_on])
+    print(game_over)
+    # print("\n \n GAME OVER")
+print(f"\n \nThe words was: {random_word }")
