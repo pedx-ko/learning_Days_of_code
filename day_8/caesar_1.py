22dda60616fba4a4fda9112e90f8ff1a44434612
pedx-ko <pedrxr2@gmail.com>
2024-06-26 12:30:17 +0200

    little changes

--- day_8/caesar_1.py
+++ day_8/caesar_1.py
@@ -6,59 +6,85 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
+# modus = 1
+# text = "el zapote es bueno par ala salud"
+# text = "cj xynmrc cq zsclm nyp yjy qyjsb"
+# shift = 2
+
+
 # using the weaparound function to avoid error aout of range
 def wraparound(index):
     return alphabet[index % len(alphabet)]
 
-# funtion to encript text
+# index_to_lookup = 26  # an invalid index
+# letter = wraparound(index_to_lookup)
 
 
-def encryption(text=str, shift_Amound=int, modus=str):
-    '''functions allow to encript a text'''
-
+# encoding
+def encrypt(text=str, shift=int):
     mesage_enco = ""
-    if modus == "decode":
-        shift_Amound *= -1
     for te in text.lower():
         if te in alphabet:
-            in_word = alphabet.index(te) + shift_Amound
+            in_word = alphabet.index(te) + shift
             mesage_enco += wraparound(in_word)
         else:
             mesage_enco += te
-    print(f"Your {modus} Message is: \n{mesage_enco}")
+    print(f"Encrypted message: \n{mesage_enco}")
 
 
+# decoding
+def decrypt(text_encoded=str, shift=int):
+    mesage_enco = ""
+    for te in text_encoded.lower():
+        if te in alphabet:
+            in_word = alphabet.index(te) - shift
+            mesage_enco += wraparound(in_word)
+        else:
+            mesage_enco += te
+    print(f"Nomarl message: \n{mesage_enco}")
+
+
+# conditions to look wich one must
+gain = True
 asking = True
+# while gain:
 while asking:
     modus = input(
-        "\nType 'encode' to encrypt, type number 'decode' to decrypt:\n").lower()
-    # try:
+        "Type 'encode' to encrypt, type number 'decode' to decrypt:\n").lower()
     if modus == "encode" or modus == "decode":
-        # print(modus)
-        print(
-            "\nplease type 'encode' if want to encode or 'decode' if want to decode\n")
+        # modus = int(modus)
+        print(modus)
+        print("please provide 1 if want to encode or '2' if want to decode\n")
         asking = False
         os.system('cls')
         inpus = True
         while inpus:
             try:
-                shift = int(input("\nType the shift number:\n"))
+                shift = int(input("Type the shift number:\n"))
                 inpus = False
-                text = input(
-                    "\nType your message for encrypting:\n").lower()
-                os.system('cls')
-                encryption(text=text, shift_Amound=shift, modus=modus)
-                inpus = False
+
+                if modus == "encode":
+                    text = input(
+                        "Type your message for encrypting:\n").lower()
+                    os.system('cls')
+                    encrypt(text=text, shift=shift)
+                    inpus = False
+                if modus == "dencode":
+                    text = input(
+                        "Type your message for decrypting:\n").lower()
+                    os.system('cls')
+                    decrypt(text_encoded=text, shift=shift)
+                    inpus = False
             except ValueError:
                 print("Invalid input. Please try again.\n")
                 sleep(3)
                 os.system('cls')
+
         otra = True
-        # asking if want to keep going or stop
         while otra:
             try:
                 agan = int(
-                    input("\type '1' you want to try again or '2' if not\n"))
+                    input("\nDo you want to try again if yes put:'1' if no: '2'\n"))
                 otra = False
                 if agan == 1 or agan == 2:
                     os.system('cls')
@@ -66,7 +92,6 @@ while asking:
                         asking = True
                     elif agan == 2:
                         asking = False
-                        print("Bye")
                 else:
                     print("vaule must be '1' or '2'")
                     otra = False
