71ca7c42c57797bf5e446f2fb99879cb70c1ecf5
pedx-ko <pedrxr2@gmail.com>
2024-06-28 11:05:55 +0200

    Improved code readability with comments

--- day_9/dict_list_exe.py
+++ day_9/dict_list_exe.py
@@ -1,3 +1,5 @@
+'adding a new dict'
+
 list_of_countrys = [
     {"country": "France", "visits": 5, "cities": [
         "Paris", "Lyon", "Marseille"]},
@@ -5,60 +7,16 @@ list_of_countrys = [
         "Berlin", "Hamburg", "Munich"]},
 ]
 
-
+# Adding new country to the list of countries
 add_new = ("Italy", 3, ["Rome", "Milan", "Venice"])
 
-# add_new = list(add_new)
-# print(add_new)
-
-# for i in add_new:
-#     list_of_countrys.append(add_new)
-# #     # print(i)
-# print(list_of_countrys)
-# print(add_new[0])
-
-
-# -----------------try with for loop
-add_new = list(add_new)
+add_new = list(add_new)  # Convert tuple to list for easier manipulation
 x = 0
 disi = {}
-for i in list_of_countrys[0]:  # range(len(add_new)):
-    key = i
-    elem = add_new[x]
-    # print(f"info : {i}  {add_new[x]}")
+
+for key in list_of_countrys[0]:
     x += 1
-    disi.update({key: elem})
-# -----------------------
+    # Create a new dictionary with the country data
+    disi.update({key: add_new[x]})
 
-# # -----------------try with for loop 2
-# disi_list = {}
-# add_new = list(add_new)
-# x = 0
-# disi = {}
-# for i in list_of_countrys[0]:  # range(len(add_new)):
-#     key = i
-#     elem = add_new[x]
-#     # print(f"info : {i}  {add_new[x]}")
-#     loca = f"{i}: {add_new[x]}"
-#     disi_list[]
-#     print(loca)
-#     x += 1
-# # -----------------------
-
-    # list_of_countrys[0][i] = "laca"
-
-    # country = list_of_countrys[i]=
-
-# # try with manual --------------------
-
-# disi = {
-#     "country": add_new[0],
-#     "visits": add_new[1],
-#     "cities": add_new[2],
-
-# }
-
-# #---------
-list_of_countrys.append(disi)
-
-print(list_of_countrys)
+list_of_countrys.append(disi)  # Adding dict to the main list of countries
