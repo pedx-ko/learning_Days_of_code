427ef6ae7992c5f64100b1513bff9e2a8017866c
pedx-ko <pedrxr2@gmail.com>
2024-07-01 10:21:33 +0200

    fix error out of range.  Improved code readability with indentation

--- day_9/dict_list_exe.py
+++ day_9/dict_list_exe.py
@@ -1,5 +1,3 @@
-'adding a new dict'
-
 list_of_countrys = [
     {"country": "France", "visits": 5, "cities": [
         "Paris", "Lyon", "Marseille"]},
@@ -7,16 +5,19 @@ list_of_countrys = [
         "Berlin", "Hamburg", "Munich"]},
 ]
 
+
+def addin_new_contry(add_new):
+    add_new = list(add_new)  # Convert tuple to list for easier manipulation
+    x = 0
+    disi = {}
+    for key in list_of_countrys[0]:
+        # Create a new dictionary with the country data
+        disi.update({key: add_new[x]})
+        x += 1
+    list_of_countrys.append(disi)  # Adding dict to the main list of countries
+    print(list_of_countrys)
+
+
 # Adding new country to the list of countries
 add_new = ("Italy", 3, ["Rome", "Milan", "Venice"])
-
-add_new = list(add_new)  # Convert tuple to list for easier manipulation
-x = 0
-disi = {}
-
-for key in list_of_countrys[0]:
-    x += 1
-    # Create a new dictionary with the country data
-    disi.update({key: add_new[x]})
-
-list_of_countrys.append(disi)  # Adding dict to the main list of countries
+addin_new_contry(add_new)
