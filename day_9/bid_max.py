86198369c244645bdbbf071d0d4a7ac1ae255067
pedx-ko <pedrxr2@gmail.com>
2024-07-01 11:15:14 +0200

    create a script that takes participants and bid to discover the maximal bidder winner.

---
+++ day_9/bid_max.py
@@ -0,0 +1,47 @@
+from email.mime import image
+import os
+import random
+from time import sleep
+from imagg import Intro_im
+
+print(Intro_im)
+
+List_of_participant = []
+maximmales = 0
+more_p = True
+winner = ""
+while more_p:
+    participant = {}
+    name = input("please put your name: ")
+
+    trato = True
+    while trato:
+        try:
+            # bid = int(input("put you bid in numbers: "))
+            bid = random.randint(1, 500)
+            trato = False
+        except ValueError:
+            print("Please provide a good data")
+            sleep(1)
+            os.system('cls')
+    participant.update({"name": name, "bid": bid})
+    List_of_participant.append(participant)
+
+    trato = True
+    while trato:
+        respuesta = input("\nAre there more Bidders? Type 'yes' or 'no'\n")
+        if respuesta == "yes" or respuesta == "no":
+            trato = False
+    if respuesta.lower() == "no":
+        more_p = False
+    os.system('cls')
+
+    # print(f"\n\n{List_of_participant}\n\n")
+for bides in List_of_participant:
+    bids = bides["bid"]
+    if bids > maximmales:
+        maximmales = bids
+        winner = bides["name"]
+
+print(
+    f"\n The winner is {winner.capitalize()} with the the bid : {maximmales}$")
---
+++ day_9/imagg.py
@@ -0,0 +1,94 @@
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+
+Intro_im = '''
+
+                                                                                                                                                
+                                                                                                                                                
+HHHHHHHHH     HHHHHHHHH                                                            PPPPPPPPPPPPPPPPP   DDDDDDDDDDDDD       XXXXXXX       XXXXXXX
+H:::::::H     H:::::::H                                                            P::::::::::::::::P  D::::::::::::DDD    X:::::X       X:::::X
+H:::::::H     H:::::::H                                                            P::::::PPPPPP:::::P D:::::::::::::::DD  X:::::X       X:::::X
+HH::::::H     H::::::HH                                                            PP:::::P     P:::::PDDD:::::DDDDD:::::D X::::::X     X::::::X
+  H:::::H     H:::::H    aaaaaaaaaaaaa  nnnn  nnnnnnnn       ggggggggg   ggggg       P::::P     P:::::P  D:::::D    D:::::DXXX:::::X   X:::::XXX
+  H:::::H     H:::::H    a::::::::::::a n:::nn::::::::nn    g:::::::::ggg::::g       P::::P     P:::::P  D:::::D     D:::::D  X:::::X X:::::X   
+  H::::::HHHHH::::::H    aaaaaaaaa:::::an::::::::::::::nn  g:::::::::::::::::g       P::::PPPPPP:::::P   D:::::D     D:::::D   X:::::X:::::X    
+  H:::::::::::::::::H             a::::ann:::::::::::::::ng::::::ggggg::::::gg       P:::::::::::::PP    D:::::D     D:::::D    X:::::::::X     
+  H:::::::::::::::::H      aaaaaaa:::::a  n:::::nnnn:::::ng:::::g     g:::::g        P::::PPPPPPPPP      D:::::D     D:::::D    X:::::::::X     
+  H::::::HHHHH::::::H    aa::::::::::::a  n::::n    n::::ng:::::g     g:::::g        P::::P              D:::::D     D:::::D   X:::::X:::::X    
+  H:::::H     H:::::H   a::::aaaa::::::a  n::::n    n::::ng:::::g     g:::::g        P::::P              D:::::D     D:::::D  X:::::X X:::::X   
+  H:::::H     H:::::H  a::::a    a:::::a  n::::n    n::::ng::::::g    g:::::g        P::::P              D:::::D    D:::::DXXX:::::X   X:::::XXX
+HH::::::H     H::::::HHa::::a    a:::::a  n::::n    n::::ng:::::::ggggg:::::g      PP::::::PP          DDD:::::DDDDD:::::D X::::::X     X::::::X
+H:::::::H     H:::::::Ha:::::aaaa::::::a  n::::n    n::::n g::::::::::::::::g      P::::::::P          D:::::::::::::::DD  X:::::X       X:::::X
+H:::::::H     H:::::::H a::::::::::aa:::a n::::n    n::::n  gg::::::::::::::g      P::::::::P          D::::::::::::DDD    X:::::X       X:::::X
+HHHHHHHHH     HHHHHHHHH  aaaaaaaaaa  aaaa nnnnnn    nnnnnn    gggggggg::::::g      PPPPPPPPPP          DDDDDDDDDDDDD       XXXXXXX       XXXXXXX
+                                                                      g:::::g                                                                   
+                                                          gggggg      g:::::g                                                                   
+                                                          g:::::gg   gg:::::g                                                                   
+                                                           g::::::ggg:::::::g                                                                   
+                                                            gg:::::::::::::g                                                                    
+                                                              ggg::::::ggg                                                                      
+                                                                 gggggg                                                                         
+'''
+
+
+game_over = '''   ____                         ___                 
+  / ___| __ _ _ __ ___   ___   / _ \__   _____ _ __ 
+ | |  _ / _` | '_ ` _ \ / _ \ | | | \ \ / / _ \ '__|
+ | |_| | (_| | | | | | |  __/ | |_| |\ V /  __/ |   
+  \____|\__,_|_| |_| |_|\___|  \___/  \_/ \___|_|   
+                                                    '''
